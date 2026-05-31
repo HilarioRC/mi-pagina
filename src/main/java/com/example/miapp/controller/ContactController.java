d8d94f4b2f95efd6687a4f82c80cff26d089d0cb
hilario reyes <reyeshilario09@gmail.com>
2026-05-30 22:19:54 -0600

    Inicializar proyecto portafolio Spring Boot MVC

---
+++ src/main/java/com/example/miapp/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.example.miapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ContactController {
+
+    @GetMapping("/contacto")
+    public String contact() {
+        return "contact";
+    }
+}
